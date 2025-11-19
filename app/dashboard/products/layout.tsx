25b61c68f1080a56700230c40fbd0e64e52848c9
asfari7 <muhammadasnafi4@gmail.com.com>
2025-11-19 19:17:22 +0700

    add front end for feature product

--- app/dashboard/products/layout.tsx
+++ app/dashboard/products/layout.tsx
@@ -1,5 +1,9 @@
 function layout({ children }: { children: React.ReactNode }) {
-  return <section>{children}</section>
+  return (
+    <section className='m-4 p-4 bg-muted/50 min-h-screen rounded-xl'>
+      {children}
+    </section>
+  )
 }
 
 export default layout
--- app/dashboard/products/page.tsx
+++ app/dashboard/products/page.tsx
@@ -1,3 +1,5 @@
+import { PaginationProduct } from "@/components/pagination"
+import { Button } from "@/components/ui/button"
 import Link from "next/link"
 
 const product = "await getProduct();"
@@ -5,10 +7,48 @@ const product = "await getProduct();"
 export default function page() {
   return (
     <section>
-      <h2>List Products Page</h2>
-      <div>
-        {/* Example product link */}
-        <Link href={`/dashboard/products/${product}`}>Product 1</Link>
+      <div className='mb-4'>
+        <h2 className='text-2xl mt-2 mb-4 font-semibold'>Data Produk</h2>
+        <Link href='/dashboard/products/create'>
+          <Button className='cursor-pointer'>Tambah</Button>
+        </Link>
+      </div>
+      <div className='bg-muted/50 min-h-screen flex-1 rounded-xl md:min-h-min'>
+        <div className='grid gap-2 auto-rows-min grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
+          <Link href={`/dashboard/products/${product}`}>
+            <div className='p-2 border-2 rounded-lg'>
+              <div className='bg-blue-400 aspect-square rounded-lg'>
+                {/* Gambar produk */}
+              </div>
+              <h3 className='my-2 text-sm md:text-md font-medium'>Produk A</h3>
+              <p className='text-lg font-semibold'>Rp 100.000</p>
+            </div>
+          </Link>
+          <div className='p-2 border-2 rounded-lg'>
+            <div className='bg-blue-400 aspect-square rounded-lg'>
+              {/* Gambar produk */}
+            </div>
+            <h3 className='my-2 text-sm md:text-md font-medium'>Produk A</h3>
+            <p className='text-lg font-semibold'>Rp 100.000</p>
+          </div>
+          <div className='p-2 border-2 rounded-lg'>
+            <div className='bg-blue-400 aspect-square rounded-lg'>
+              {/* Gambar produk */}
+            </div>
+            <h3 className='my-2 text-sm md:text-md font-medium'>Produk A</h3>
+            <p className='text-lg font-semibold'>Rp 100.000</p>
+          </div>
+          <div className='p-2 border-2 rounded-lg'>
+            <div className='bg-blue-400 aspect-square rounded-lg'>
+              {/* Gambar produk */}
+            </div>
+            <h3 className='my-2 text-sm md:text-md font-medium'>Produk A</h3>
+            <p className='text-lg font-semibold'>Rp 100.000</p>
+          </div>
+        </div>
+        <div className='my-4'>
+          <PaginationProduct />
+        </div>
       </div>
     </section>
   )
